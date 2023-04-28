cd41ef527ecf52b3ddb7ebeac1dd96ad6c79d003
Narasimman2067 <narasimmamech2067@gmail.com>
2023-04-28 10:09:38 +0530

    new commit

--- src/utils/apihelpers.js
+++ src/utils/apihelpers.js
@@ -1,4 +1,4 @@
-const host="http://localhost:8000";
+const host="https://authentication-backend-xi.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
@@ -8,9 +8,3 @@ export const recieveMessageRoute = `${host}/api/messages/getmsg`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
 
 
-// export const registerRoute=`${host}/api/auth/register`;
-// export const loginRoute=`${host}/api/auth/login`;
-// export const setAvatarRoute=`${host}/api/auth/setavatar`;
-// export const allUsersRoute=`${host}/api/auth/allusers`;
-// export const sendMessageRoute=`${host}/api/messages/addmsg`;
-// export const getAllMessageRoute=`${host}/api/messages/getmsg`;
